d042f4abae7f08183aa12c1ff66e64e8c2ec360f
prajwalhaniya <prajwalhaniya@gmail.com>
2025-08-28 10:02:16 +0530

    initial bootstrap commit

---
+++ node_thorq/src/routes/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import userController from "../controller/index.js";
+
+const router = express.Router({ mergeParams: true });
+
+router.get("/", (req, res) => {
+    res.send("This is a simple route");
+});
+
+router.get("/health", async (req, res) => {
+    const getUser = await userController.getUsers();
+    res.send(getUser);
+});
+
+
+export default router;
---
+++ node_thorq/src/server.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import appRoutes from "./routes/index.js";
+import "reflect-metadata";
+import logger from "./services/logger/index.js";
+
+import { Request, Response, NextFunction } from "express";
+
+const PORT = 3000;
+const app = express();
+
+const router = express.Router({ mergeParams: true });
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // handle the error as required
+    res.status(500).send(err.message);
+});
+
+router.use("/app", appRoutes);
+
+app.listen(PORT, () => {
+    logger.info("Server is listening on the port", PORT);
+    console.log("Server is listening on the port", PORT);
+});
+
